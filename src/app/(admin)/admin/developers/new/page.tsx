f26736d3977b76cbc2d9f59fd3c2905aafab9d34
Stanley Winata <winatastanley355@gmail.com>
2026-09-12 08:35:27 +0700

    Remove developers and moved to constant

--- src/app/(admin)/admin/developers/new/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NewDeveloperFormCard } from "./_components/new-developer-form-card";
-
-export default function Page() {
-  return <NewDeveloperFormCard />;
-}
---
+++ src/lib/developers.ts
@@ -0,0 +1,107 @@
+export const DEVELOPERS = [
+  {
+    "id": 18,
+    "created_at": "2026-01-28 07:50:17.298548",
+    "updated_at": "2026-01-28 07:50:17.298548",
+    "logo_path": "/primepro-property-3/gunung-sewu",
+    "name": "Gunung Sewu"
+  },
+  {
+    "id": 17,
+    "created_at": "2026-01-19 04:52:14.588902",
+    "updated_at": "2026-01-19 04:52:14.588902",
+    "logo_path": "/primepro-property-3/swancity",
+    "name": "Swancity"
+  },
+  {
+    "id": 16,
+    "created_at": "2026-01-19 04:51:46.172616",
+    "updated_at": "2026-01-19 04:51:46.172616",
+    "logo_path": "/primepro-property-3/mustika-land",
+    "name": "Mustika Land"
+  },
+  {
+    "id": 15,
+    "created_at": "2026-01-19 04:51:16.525585",
+    "updated_at": "2026-01-19 04:51:16.525585",
+    "logo_path": "/primepro-property-3/agung-sedayu-group",
+    "name": "Agung Sedayu Group"
+  },
+  {
+    "id": 14,
+    "created_at": "2026-01-19 04:50:51.601943",
+    "updated_at": "2026-01-19 04:50:51.601943",
+    "logo_path": "/primepro-property-3/trinland",
+    "name": "Trinland"
+  },
+  {
+    "id": 13,
+    "created_at": "2026-01-19 04:50:07.326623",
+    "updated_at": "2026-01-19 04:50:07.326623",
+    "logo_path": "/primepro-property-3/paramount-land",
+    "name": "Paramount Land"
+  },
+  {
+    "id": 12,
+    "created_at": "2026-01-19 04:49:23.804801",
+    "updated_at": "2026-01-19 04:49:23.804801",
+    "logo_path": "/primepro-property-3/lippo-group",
+    "name": "Lippo Group"
+  },
+  {
+    "id": 11,
+    "created_at": "2026-01-19 04:48:35.4727",
+    "updated_at": "2026-01-19 04:48:35.4727",
+    "logo_path": "/primepro-property-3/sumarecon-serpong",
+    "name": "Sumarecon Serpong"
+  },
+  {
+    "id": 10,
+    "created_at": "2026-01-19 04:12:03.515017",
+    "updated_at": "2026-01-19 04:12:03.515017",
+    "logo_path": "/primepro-property-3/alam-sutera",
+    "name": "Alam Sutera"
+  },
+  {
+    "id": 8,
+    "created_at": "2026-01-19 04:05:42.213912",
+    "updated_at": "2026-01-19 04:05:42.213912",
+    "logo_path": "/primepro-property-3/bsd-city",
+    "name": "BSD CITY"
+  },
+  {
+    "id": 7,
+    "created_at": "2026-01-19 04:04:56.239258",
+    "updated_at": "2026-01-19 04:04:56.239258",
+    "logo_path": "/primepro-property-3/agung-podomoro-land",
+    "name": "Agung Podomoro Land"
+  },
+  {
+    "id": 6,
+    "created_at": "2026-01-19 04:04:12.714999",
+    "updated_at": "2026-01-19 04:04:12.714999",
+    "logo_path": "/primepro-property-3/sinarmas-land",
+    "name": "Sinarmas Land"
+  },
+  {
+    "id": 5,
+    "created_at": "2026-01-19 04:03:24.643492",
+    "updated_at": "2026-01-19 04:03:24.643492",
+    "logo_path": "/primepro-property-3/ciputra-grup",
+    "name": "Ciputra Grup"
+  },
+  {
+    "id": 4,
+    "created_at": "2026-01-19 04:01:43.588482",
+    "updated_at": "2026-01-19 04:01:43.588482",
+    "logo_path": "/primepro-property-3/paradise-serpong-city",
+    "name": "Paradise Serpong City"
+  },
+  {
+    "id": 3,
+    "created_at": "2026-01-19 04:00:44.976086",
+    "updated_at": "2026-01-19 04:00:44.976086",
+    "logo_path": "/primepro-property-3/gardens-at-candi-sawangan",
+    "name": "Gardens at Candi Sawangan"
+  }
+]
