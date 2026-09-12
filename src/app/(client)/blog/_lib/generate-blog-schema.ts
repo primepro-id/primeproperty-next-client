83b274f396b445aff8ce2eb599924ecb5095de34
Stanley Winata <winatastanley355@gmail.com>
2026-09-12 08:33:28 +0700

    Prettier

--- src/app/(client)/blog/_lib/generate-blog-schema.ts
+++ src/app/(client)/blog/_lib/generate-blog-schema.ts
@@ -14,7 +14,9 @@ export function generateBlogSchema(article: Article) {
         headline: article.title,
         datePublished: new Date(article._publishedAt).toISOString(),
         dateModified: new Date(article._updatedAt).toISOString(),
-        thumbnailUrl: article?.thumbnail?.url ?? `${env.NEXT_PUBLIC_HOST_URL}${article?.thumbnailImagePath}`,
+        thumbnailUrl:
+          article?.thumbnail?.url ??
+          `${env.NEXT_PUBLIC_HOST_URL}${article?.thumbnailImagePath}`,
         articleSection: article.seo.title,
         description: article.seo.description,
         author: {
@@ -25,7 +27,9 @@ export function generateBlogSchema(article: Article) {
         },
         image: {
           "@type": "ImageObject",
-          url: article?.thumbnail?.url ?? `${env.NEXT_PUBLIC_HOST_URL}${article?.thumbnailImagePath}`,
+          url:
+            article?.thumbnail?.url ??
+            `${env.NEXT_PUBLIC_HOST_URL}${article?.thumbnailImagePath}`,
         },
         publisher: {
           "@type": "Organization",
@@ -57,7 +61,9 @@ export function generateBlogSchema(article: Article) {
             "@type": "ListItem",
             position: 3,
             name: article.title,
-            item: article?.thumbnail?.url ?? `${env.NEXT_PUBLIC_HOST_URL}${article?.thumbnailImagePath}`
+            item:
+              article?.thumbnail?.url ??
+              `${env.NEXT_PUBLIC_HOST_URL}${article?.thumbnailImagePath}`,
           },
         ],
       },
